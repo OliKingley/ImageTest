69d100fec9d65153d5d8c4c229c3b7cf3c8d2772
Oliver <opoku.oliver@gmail.com>
2017-04-19 23:15:52 +0200

    Image works

--- ImageTest/ViewController.swift
+++ ImageTest/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBAction func ImportPic(_ sender: Any) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        image.allowsEditing = true
+        
+        self.present(image, animated: true) {
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            ImageView.image = image
+        }
+        
+        else {
+            
+            //Error Message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
